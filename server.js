73b3adb390d5e9a5e10619eb0a6849ba58fd74f8
anushka maheshwari <anushkamaheshwari02@gmail.com>
2023-12-28 14:04:23 +0530

    server

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// Read the data from the JSON file
+const jsonData = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
 app.post('/processNumbers', (req, res) => {
   const { numbers } = req.body;
 
-  // Process the numbers (remove odd numbers)
-  const resultNumbers = numbers.filter((num) => num % 2 === 0);
+  // Process the numbers and get corresponding values from the JSON file
+  const resultValues = numbers.map((num) => {
+    // Determine the dictionary key and value key based on the input number
+    const dictionaryKey = `dictionary${num % 2 + 1}`;
+    const valueKey = `value${num % 3 + 1}`;
 
-  res.json(resultNumbers);
+    // Retrieve the corresponding value from the JSON file
+    return jsonData[dictionaryKey][valueKey];
+  });
+
+  res.json(resultValues);
 });
 
 app.listen(port, () => {
